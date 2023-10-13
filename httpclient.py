0ffe41755ca95a9ba9e754fe6293744e40ac70d5
pr0d1gypumkn <akeshta@ualberta.ca>
2023-10-13 14:18:54 -0600

    finished all tests except testInternetGets()

--- httpclient.py
+++ httpclient.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python3
-# coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#  Copyright 2023 Ahmed Keshta
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 
 # Do not use urllib's HTTP GET and POST mechanisms.
 # Write your own HTTP GET and POST
@@ -57,52 +56,81 @@ class HTTPClient(object):
 
     # read everything from the socket
     def recvall(self, sock):
-        buffer = bytearray()
-        done = False
-        while not done:
-            part = sock.recv(1024)
-            if (part):
-                buffer.extend(part)
-            else:
-                done = not part
-        return buffer.decode('utf-8')
+        response = b''
+        body = False
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                break
+            response += data
+            if b'\r\n\r\n' in response and body:
+                break
+            elif b'\r\n\r\n' in response:
+                body = True
+        return response.decode()
 
     def GET(self, url, args=None):
         code = 500
-        
+        body = ''
         url = parse.urlparse(url)
         path = url.path
+        if not path:
+            path = "/"
+        elif not path.endswith("/") and not path.endswith(".html") and not path.endswith(".css"):
+            path += "/"
+        
+        
         host = url.hostname
-        self.connect(host, 8080)
-        request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n"
+        port = url.port
+        if not port:
+            port = 80
+
+        self.connect(host, port)
+        request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n"
         self.sendall(request)
+        self.socket.shutdown(socket.SHUT_WR)
         response = self.recvall(self.socket)
+        
         self.close()
+        status = 0
+
+        status = int(response.split("\r\n")[0].split(" ")[1])
         response = response.split("\r\n\r\n")
-        body = response[1]
-        code = int(response[0].split(" ")[1])
+        if len(response) > 1:
+            body = response[1]
+        code = int(status)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
+        body = ''
         if args:
-            args = parse.urlencode(args)
+            vars = parse.urlencode(args)
         else:
-            args = ""
+            vars = parse.urlencode({})
         url = parse.urlparse(url)
         path = url.path
-        host = url.hostname
-        request = f"POST {path} HTTP/1.1\r\n\
-                   Host: {host}\r\n\
-                   Content-Type: application/x-www-form-urlencoded\r\n\
-                   Content-Length: {len(args)}\r\n\r\n{args}"
+        if not path.endswith("/"):
+            path += "/"
         
-        self.connect("localhost", 8080)
+        port = url.port
+        if not port:
+            port = 80
+        host = url.hostname
+        request = f"POST {path} HTTP/1.1\r\n" + \
+                   f"Host: {host}\r\n" + \
+                   "Content-Type: application/x-www-form-urlencoded\r\n" + \
+                   "Connection: close\r\n" + \
+                   f"Content-Length: {len(vars)}\r\n\r\n{vars}\r\n"
+        
+        self.connect(host, port)
         self.sendall(request)
+        self.socket.shutdown(socket.SHUT_WR)
         response = self.recvall(self.socket)
         self.close()
         response = response.split("\r\n\r\n")
-        body = response[1]
+        if len(response) > 1:
+            body = response[1]
         code = int(response[0].split(" ")[1])
         return HTTPResponse(code, body)
 
